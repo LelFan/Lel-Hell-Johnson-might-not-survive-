b00391719e84a8451e43e39a662f6e8d1c75404d
LelFan <fridgepudding@gmail.com>
2023-04-27 12:25:44 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,8 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
+let gameState = "menu";
+ 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
