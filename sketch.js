fcbfe417e12d61cd0526cd0472937d4732e39f18
LelFan <fridgepudding@gmail.com>
2023-05-08 15:19:19 -0600

    fixed up menu setup
    Still a lot to fix up, but stuff is starting to work

--- sketch.js
+++ sketch.js
@@ -4,47 +4,71 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let gameState = "menu";
-let plays, controls;
-
+let gameState = "menuSetup";
+let plays;
+let controls;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   textAlign(CENTER);
+  menuSetup();
+  gamePlaySetup();
 }
 
 function draw() {
   background(255);
+
   if (gameState === "menu") {
     menu();
   }
   if (gameState ==="play") {
-    play();
+    gamePlay();
   } 
 }
 
 function menu() {
-  textSize(40);
   text("Johnson's Final Stand",width/2,height*0.25);
+  if (plays.mouse.hovering()) {
+    plays.color = "black";
+  }
+  else {
+    plays.color = "white";
+  }
+  if (plays.mouse.presses()){
+    gameState = "play";
+    plays.visible = false;
+    plays.collider = "n";
+    controls.visible = false;
+    controls.collider = "n";
+  }
+  if (controls.mouse.presses()){
+    gameState = "controls";
+    plays.visible = false;
+    controls.visible = false;
+  }
+  if (controls.mouse.hovering()) {
+    controls.color = "black";
+  }
+  else {
+    controls.color = "white";
+  }
+}
+
+function menuSetup(){
+  textSize(40);
   plays = new Sprite(width/2,height/2,200,100,"s");
   plays.color = "white";
   plays.text = "Play";
-  if (plays.mouse.hovering()) {
-    plays.color = "black";
-    console.log("what");
-  }
-
   controls = new Sprite(width/2,height*0.7,200,100,"s");
   controls.color = "white";
   controls.text = "Controls";
-  if (controls.mouse.presses()){
-    gameState = "controls";
-  }
+  gameState = "menu";
 }
 
-function play() {
-  let player;
+function gamePlaySetup() {
+  let player, floor;
   player = new Sprite(width/4,height*0.8,50,"d"); //will change to an actual player later
+  floor = new Sprite(width/2,height*0.9,width,100, "d");
 }
-// create a menu set up state
+// create a gameplay set up state
